9f77a26ce5f184525d9e808a3005154d129fba6a
REVAN <revanescence33@gmail.cpm>
2024-03-18 14:14:56 +0530

    intro

---
+++ intro.cpp
@@ -0,0 +1,23 @@
+// creted on 1980
+
+// software, games, browser,compiler
+
+// simula + c/
+
+#include <iostream>
+
+int main()
+{
+    std:: cout<<"Hello world\n";
+    // return 0;
+}
+
+
+// #inclsude <cstdio>
+// using namespace std;
+
+// int manin(){
+//     puts("here")
+//     return 0;
+
+// // }
